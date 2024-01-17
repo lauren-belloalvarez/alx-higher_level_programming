0bf3e4d310b2ab4e95050599896c6252bb5f6e55
lauren-belloalvarez <lauren2104ba@yahoo.com>
2024-01-17 06:07:20 -0800

    Add Corrected tasks

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,22 +3,22 @@
 import json
 import csv
 
+
 class Base:
-	"""Defines base model classess for entire project
-	Attributes:
-		__nb_objects : int, number of bases
-	"""
+    """Defines base model classess for entire project
+        Attributes:
+                   __nb_objects : int, number of bases
+        """
 
-	__nb_objects = 0
+    __nb_objects = 0
 
-	def __init__(self, id=None):
-		"""Class Constructor that initialises new Base
-		Arguments:
-		id : int, identity of new base
-		"""
-
-		if id is not None:
-			self.id = id
-		else:
-			Base.__nb_objects += 1
-			self.id = Base. __nb_objects
+    def __init__(self, id=None):
+        """Class Constructor that initialises new Base
+        Arguments:
+                    id : int, identity of new base
+                    """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base. __nb_objects
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,76 +2,75 @@
 """Defining rectangle class"""
 from models.base import Base
 
+
 class Rectangle(Base):
-	""" Defining rectangle by inheriting from Base class"""
+    """ Defining rectangle by inheriting from Base class"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        """Creating new rectangle.
+        Arguments:
+        width : width of rectangle
+        height : height of rectangle
+        x : x co-ord. of rectangle
+        y : y co-ord. of rectangle
+        id : identity of rectangle
+        """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
-	def __init__(self, width, height, x=0, y=0, id=None):
-		super().__init__(id)
-		"""Creating new rectangle.
-		Arguments:
-		width : width of rectangle
-		height : height of rectangle
-		x : x co-ord. of rectangle
-		y : y co-ord. of rectangle
-		id : identity of rectangle
-		"""
-		self.width = width
-		self.height = height
-		self.x = x
-		self.y = y
+    """Getter and Setters"""
+    @property
+    def width(self):
+        return self.__width
 
-	"""Getter and Setters"""
-	@property
-	def width(self):
-		return self.__width
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
 
-	@width.setter
-	def width(self, value):
-		if type(value) != int:
-			raise TypeError("width must be an integer")
-		if value <= 0:
-			raise ValueError("width must be > 0")
-		self.__width = value
-		
-	@property
-	def height(self):
-		return self.__height
+    @property
+    def height(self):
+        return self.__height
 
-	@height.setter
-	def height(self, value):
-		if type(value) != int:
-			raise TypeError("height must be an integer")
-		if value <= 0:
-			raise ValueError("height must be > 0")
-		self.__height = value
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
 
-	@property
-	def x(self):
-		return self.__x
+    @property
+    def x(self):
+        return self.__x
 
-	@x.setter
-	def x(self, value):
-		if type(value) != int:
-			raise TypeError("x must be an integer")
-		if value < 0:
-			raise ValueError("x must be >= 0")
-		self.__x = value
+    @x.setter
+    def x(self, value):
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
 
-	@property
-	def y(self):
-		return self.__y
+    @property
+    def y(self):
+        return self.__y
 
-	@y.setter
-	def y(self, value):
-		if type(value) != int:
-			raise TypeError("y must be an integer")
-		if value < 0:
-			raise ValueError("y must be >= 0")
-		self.__y = value
+    @y.setter
+    def y(self, value):
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
 
-	def area(self):
-		ra = self.height * self.width
-		return ra
+    def area(self):
+        return self.__width * self.__height
 
-	def display(self):
-		pass
+    def display(self):
+        pass
